43c170c7b2fe6658846a8f19d556ee7c4ab212c8
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2022-04-26 10:01:06 +0300

    Update C_Algorithms_HW7_L10.c

--- C_Algorithms_HW7_L10.c
+++ C_Algorithms_HW7_L10.c
@@ -12,6 +12,94 @@
 // 4 - стек на основе односвязного списка
 // 5 - очередь на основе двухсвязного списка
 
+typedef struct Node {
+	int data;
+	struct Node *next;
+}; Node;
+
+typedef struct List {
+	Node* head;
+	int size;
+}; List;
+
+void init(List* lst) {
+	lst->head = NULL;
+	lst->size = 0;
+}
+
+void insertList(List *lst, int data) {
+	Node* newOne = (Node*) malloc(sizeof(Node));
+	newOne->data = data;
+	newOne->next = NULL;
+
+	Node* currentOne = lst->head;
+	if (currentOne == NULL) {
+		lst->head = newOne;
+		lst->size++;
+		return;
+	}
+	else {
+		while (currentOne->next != NULL) {
+			currentOne = currentOne->next;
+		}
+		currentOne->next = newOne;
+		lst->size++;
+	}
+}
+
+// удаление по значению:
+Node* removeNode(List* lst, int data) {
+	Node* currentOne = lst->head;
+	Node* previousOne = NULL;
+	if (currentOne == NULL) {
+		return NULL;
+	}
+
+	while (currentOne->next != NULL && currentOne->data != data) {
+		previousOne = currentOne;
+		currentOne = currentOne->next;
+	}
+
+	if (currentOne->data != data) {
+		return NULL;
+	}
+
+	if (currentOne == lst->head) {
+		lst->head = currentOne->next;
+		lst->size--;
+		return currentOne;
+	}
+
+	previousOne->next = currentOne->next;
+	lst->size--;
+	return currentOne;
+}
+
+void printNode(Node* n) {
+	if (n == NULL) {
+		printf("[]");
+		return;
+	}
+	printf("[%d]", n->data);
+}
+
+void printList(List* lst) {
+	Node* currentOne = lst->head;
+	if (currentOne == NULL) {
+		printNode(currentOne);
+	}
+	else {
+		do {
+			printNode(currentOne);
+			currentOne = currentOne->next;
+		} while (currentOne != NULL);
+	}
+	printf("Size: %d \n", lst->size);
+}
+
+
+
+
 
 
 // Home work:
@@ -23,8 +111,22 @@
 // 3 - Реализуйте алгоритм, который определяет, отсортирован ли связный список.
 
 int main(const int argc, const char** argv) {
+	// Class work:
+	// Выделим участок памяти для LIST:
+	List* lst = (List*)malloc(sizeof(List));
+	init(lst);
+	printList(lst);
 
+	insertList(lst, 1);
+	printList(lst);
 
+	insertList(lst, 5);
+	printList(lst);
 
+	insertList(lst, 3);
+	printList(lst);
 
+	printNode(removeNode(lst, 1)); printf("<- removed mode\n");
+	printList(lst);
+	return 0;
 }
